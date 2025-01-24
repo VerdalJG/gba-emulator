7981d26215142ee16e5eb7560a4a94c26cf86d2f
DarcinDC <darcindc@gmail.com>
2025-01-24 16:55:32 +0000

    Made SDL actually work now

--- Makefile
+++ Makefile
@@ -4,7 +4,11 @@ all:
 	./build/src/Debug/GBA-Emulator.exe
 
 build:
+	cmake -S . -B build
 	cmake --build build
+
+clean:
+	rm -rf build
 	
 run:
 	./build/src/Debug/GBA-Emulator.exe
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+#include <SDL3/SDL.h>
+
 int main(int, char**){
     std::cout << "Hello, from GBA-Emulator!\n";
+
+    std::cout << "ABS: " << SDL_abs(-1) << std::endl;
 }
